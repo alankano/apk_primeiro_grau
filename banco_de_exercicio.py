c85c0ce502d32ef81204ccac2afda37fd2e221d7
Alan Kano <38501151+alankano@users.noreply.github.com>
2020-11-24 00:03:15 -0300

    Add files via upload

---
+++ banco_de_exercicio.py
@@ -0,0 +1,279 @@
+dict_exercicios = {
+    0 : {
+        'enunciado': '3/5 de um número somados a 1/2 é igual a 2/3 desse mesmo número. Indique a opção que apresenta esse número.',
+        'alternativas' :{'a' : '0',
+                         'b' : '1',
+                         'c' : '20/33',
+                         'd' : '33/20',
+                         'e' : '15/2'
+                         },
+        'resposta_correta' : 'e'
+    },
+    1 : {
+        'enunciado': 'Qual a resposta da equação x + 30 = 40?',
+        'alternativas' :{'a' : '13,8',
+                         'b' : '10',
+                         'c' : '15',
+                         'd' : '23',
+                         'e' : '12'
+                         },
+        'resposta_correta' : 'b'
+    },
+    2 : {
+        'enunciado': 'Qual a resposta da equação 30-20+2x=10?',
+        'alternativas' :{'a' : '3',
+                         'b' : '4',
+                         'c' : '8',
+                         'd' : '0',
+                         'e' : '1'
+                         },
+        'resposta_correta' : 'd'
+    },
+    3 : {
+        'enunciado': 'Qual o resultado da equaçao 3x-10+13=-2x+28?',
+        'alternativas' :{'a' : '1',
+                         'b' : '4',
+                         'c' : '6',
+                         'd' : '7',
+                         'e' : '5'
+                         },
+        'resposta_correta' : 'e'
+    },
+    4 : {
+        'enunciado': 'Qual a resposta da equação -5x+45-89=-90+41?',
+        'alternativas' :{'a' : '3',
+                         'b' : '2',
+                         'c' : '6',
+                         'd' : '1',
+                         'e' : '8'
+                         },
+        'resposta_correta' : 'd'
+    },
+    5 : {
+        'enunciado': 'Qual a resposta da equação 10x-20=40+50?',
+        'alternativas' :{'a' : '12',
+                         'b' : '11',
+                         'c' : '10',
+                         'd' : '9',
+                         'e' : '3'
+                         },
+        'resposta_correta' : 'b'
+    },
+
+    6 : {
+        'enunciado': 'Numa família com 7 filhos, sou o caçula e 14 anos mais novo que o primogênito de minha mãe. Dentre os filhos, o quarto tem a terça parte da idade do irmão mais velho, acrescidos de 7 anos. Se a soma de nossas três idades é 42, então minha idade é um número',
+        'alternativas' :{'a' : 'divisível por 5.',
+                         'b' : 'divisível por 3.',
+                         'c' : 'primo.',
+                         'd' : 'par.',
+                         'e' : 'nulo.'
+                         },
+        'resposta_correta' : 'b'
+    },
+    7 : {
+        'enunciado': 'Pedro tinha x reais das suas economias. Gastou um terço no parque de diversões com os amigos. No outro dia, gastou 10 reais com figurinhas para seu álbum de jogadores de futebol. Depois saiu para lanchar com seus colegas na escola gastando mais 4/5 do que ainda tinha e ficou ainda com um troco de 12 reais. Qual o valor de x em reais?',
+        'alternativas' :{'a' : '75',
+                         'b' : '80',
+                         'c' : '90',
+                         'd' : '100',
+                         'e' : '105'
+                         },
+        'resposta_correta' : 'e'
+    },
+    8 : {
+        'enunciado': 'Carlos e Manoela são irmãos gêmeos. A metade da idade de Carlos mais um terço da idade de Manoela é igual a 10 anos. Qual é a soma das idades dos dois irmãos?',
+        'alternativas' :{'a' : '23',
+                         'b' : '24',
+                         'c' : '25',
+                         'd' : '27',
+                         'e' : '26'
+                         },
+        'resposta_correta' : 'b'
+    },
+    9 : {
+        'enunciado': 'Rosinha pagou R$ 67,20 por uma blusa que estava sendo vendida com desconto de 16%. Quando suas amigas souberam, correram para a loja e tiveram a triste notícia que o desconto já havia acabado. O preço encontrado pelas amigas de Rosinha foi:',
+        'alternativas' :{'a' : 'R$ 70,00.',
+                         'b' : 'R$ 75,00.',
+                         'c' : 'R$ 80,00.',
+                         'd' : 'R$ 85,00.',
+                         'e' : 'R$ 77,50'
+                         },
+        'resposta_correta' : 'c'
+    },
+    10 : {
+        'enunciado': 'Um pacote do biscoito Saboroso custa R$ 1,25. Se João comprou N pacotes desse biscoito gastando R$ 13,75, o valor de N é igual a:',
+        'alternativas' :{'a' : '11',
+                         'b' : '12',
+                         'c' : '13',
+                         'd' : '14',
+                         'e' : '15'
+                         },
+        'resposta_correta' : 'a'
+    },
+    11 : {
+        'enunciado': 'Um Professor gasta 1 terço do seu salário com alimentação, 1 meio com moradia e ainda lhe sobram R$ 1.200,00. Qual é o salário desse professor?',
+        'alternativas' :{'a' : 'R$ 2.200,00',
+                         'b' : 'R$ 7.200,00',
+                         'c' : 'R$ 7.000,00',
+                         'd' : 'R$ 6.200,00',
+                         'e' : 'R$ 5.400,00'
+                         },
+        'resposta_correta' : 'b'
+    },
+    11 : {
+        'enunciado': "Uma fábrica de panelas opera com um custo fixo mensal de R$ 9 800,00 e um custo variável por panela de R$ 45,00. Cada panela é vendida por R$ 65,00. Seja x a quantidade que deve ser produzida e vendida mensalmente para que o lucro mensal seja igual a 20% da receita. A soma dos algarismos de x é:",
+        'alternativas' :{'a' : '6',
+                         'b' : '5',
+                         'c' : '4',
+                         'd' : '3',
+                         'e' : '2'
+                         },
+        'resposta_correta' : 'b'
+    },
+    12 : {
+        'enunciado': "Um estacionamento X cobra 6 centavos por minuto, até um valor máximo de R$40,00. Outro estacionamento Y cobra uma tarifa fixa de R$5,00 por qualquer período até completar 1 hora, e, a partir daí, cobra 5 centavos por minuto extra. Com base nesses valores, só será mais vantajoso deixar o carro em Y do que em X, se for por um período de",
+        'alternativas' :{'a' : '2h20min até 11h40min.',
+                         'b' : '2h20min até 13h20min.',
+                         'c' : '3h20min até 12h40min.',
+                         'd' : '3h20min até 13h20min.',
+                         'e' : '4h40min até 12h40min.'
+                         },
+        'resposta_correta' : 'c'
+    },
+    13 : {
+        'enunciado': "A nota final de um concurso é dada pela média aritmética das notas de todas as provas realizadas. Se um candidato conseguiu x notas 8, x + 1 notas 6 e x – 1 notas 5 e sua nota final foi 6,5, o número de provas que ele realizou foi:",
+        'alternativas' :{'a' : '9',
+                         'b' : '12',
+                         'c' : '6',
+                         'd' : '5',
+                         'e' : '7'
+                         },
+        'resposta_correta' : 'c'
+    },
+    14 : {
+        'enunciado': "Uma empresa A cobra R$ 80,00 por um determinado produto, mais uma taxa mensal de R$ 20,00 para manutenção. Uma empresa B cobra R$ 120,00 pelo mesmo produto, mais a taxa mensal de R$ 12,00 para manutenção. A empresa B será mais vantajosa que a A",
+        'alternativas' :{'a' : 'sempre.',
+                         'b' : 'a partir do 4º mês.',
+                         'c' : 'a partir do 7º mês.',
+                         'd' : 'a partir do 5º mês.',
+                         'e' : 'a partir do 10º mês.'
+                         },
+        'resposta_correta' : 'd'
+    },
+    15 : {
+        'enunciado': "Considerando a equação –5(3x – 8) = –45, é CORRETO afirmar que ela é equivalente a",
+        'alternativas' :{'a' : '–8x – 58 = 0',
+                         'b' : '–15x + 85 = 0',
+                         'c' : '–15x + 5 = 0',
+                         'd' : '–8x – 32 = 0',
+                         'e' : '–15x – 53 = 0'
+                         },
+        'resposta_correta' : 'b'
+    },
+    16 : {
+        'enunciado': "Se hoje Pedro tem o dobro da idade de Maria e daqui a 20 anos Maria será 10 anos mais jovem do que Pedro, qual será a idade de Pedro nessa época?",
+        'alternativas' :{'a' : '30 anos',
+                         'b' : '35 anos',
+                         'c' : '40 anos',
+                         'd' : '45 anos',
+                         'e' : '50 anos'
+                         },
+        'resposta_correta' : 'c'
+    },
+    17 : {
+        'enunciado': "A soma de três números inteiros consecutivos é 60. Qual é o produto entre esses três números?",
+        'alternativas' :{'a' : '0',
+                         'b' : '3560',
+                         'c' : '7980',
+                         'd' : '6850',
+                         'e' : '12560'
+                         },
+        'resposta_correta' : 'c'
+    },
+    18 : {
+        'enunciado': 'Sabe-se que o preço a ser pago por uma corrida de táxi inclui uma parcela fixa, que é denominada bandeirada, e uma parcela variável, que é função da distância percorrida. Se o preço da bandeirada é R$4,60 e o quilômetro rodado é R$0,96, qual a distância percorrida por um passageiro que pagou R$19,00?',
+        'alternativas' :{'a' : '15',
+                         'b' : '16',
+                         'c' : '17',
+                         'd' : '18',
+                         'e' : '19'
+                         },
+        'resposta_correta' : 'a'
+    },
+    19 : {
+        'enunciado': 'Um terreno retangular possui o comprimento cinco vezes maior que a largura. Sabendo que o perímetro desse terreno é igual a 180 metros, a largura e o comprimento medem, respectivamente:',
+        'alternativas' :{'a' : '30 m e 150 m',
+                         'b' : '75 m e 15 m',
+                         'c' : '15 m e 75 m',
+                         'd' : '150 m e 30 m',
+                         'e' : '90 m e 90 m'
+                         },
+        'resposta_correta' : 'b'
+    },
+    20 : {
+        'enunciado': 'A soma de um número com seu quíntuplo é igual ao dobro desse mesmo número somado com 40. Que número é esse?',
+        'alternativas' :{'a' : '6',
+                         'b' : '7',
+                         'c' : '8',
+                         'd' : '9',
+                         'e' : '10'
+                         },
+        'resposta_correta' : 'e'
+    },
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ minha_materia.py
@@ -0,0 +1,47 @@
+primeira_string ='''
+            Uma equação do primeiro grau é uma expressão em que o grau da incógnita é 1, isto é, o expoente da incógnita é igual a 1. Podemos representar uma equação do primeiro grau, de maneira geral, da seguinte forma:  
+
+            ax + b = 0
+            
+            Donde a e b são números reais, sendo a um valor diferente de zero (a ≠ 0) e x representa o valor desconhecido.
+            
+            O valor desconhecido é chamado de incógnita que significa "termo a determinar". As equações do 1º grau podem apresentar uma ou mais incógnitas.
+        
+        Como resolver uma equação de primeiro grau?
+        O objetivo de resolver uma equação de primeiro grau é descobrir o valor desconhecido, ou seja, encontrar o valor da incógnita que torna a igualdade verdadeira.
+        
+        Para isso, deve-se isolar os elementos desconhecidos em um dos lados do sinal de igual e os valores constantes do outro lado.
+        
+        Contudo, é importante observar que a mudança de posição desses elementos deve ser feita de forma que a igualdade continue sendo verdadeira.
+        
+        Quando um termo da equação mudar de lado do sinal de igual, devemos inverter a operação. Assim, se tiver multiplicando, passará dividindo, se tiver somando, passará subtraindo e vice-versa.
+ '''       
+        
+segunda_string = '''    
+        Exemplo
+        Qual o valor da incógnita x que torna a igualdade 8x - 3 = 5 verdadeira?
+        
+        
+        Solução
+        Para resolver a equação, devemos isolar o x. Para isso, vamos primeiro passar o 3 para o outro lado do sinal de igual. Como ele está subtraindo, passará somando. Assim:
+        
+        8x = 5 + 3
+        
+        8x = 8
+        
+        Agora podemos passar o 8, que está multiplicando o x, para o outro lado dividindo:
+        
+        x = 8/8
+        
+        x = 1
+        
+        Outra regra básica para o desenvolvimento das equações de primeiro grau determina o seguinte:
+        
+        Se a parte da variável ou a incógnita da equação for negativa, devemos multiplicar todos os membros da equação por –1. Por exemplo:
+        
+        – 9x = – 90 . (-1)
+        
+        9x = 90
+        
+        x = 10
+        '''
